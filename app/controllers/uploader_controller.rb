bcaedc6d45229472002468f204166457144043ca
kajaakajaa <rwqxh8703@yahoo.co.jp>
2020-02-17 19:56:03 +0900

    add_15mac_2-17

--- app/controllers/uploader_controller.rb
+++ app/controllers/uploader_controller.rb
@@ -5,16 +5,10 @@ class UploaderController < ApplicationController
   def form
   end
 
-  # def upload
-  #   @upload_file = UploadFile.new( params.require(:upload_file).permit(:name, :file) )
-  #   @upload_file.save
-  #   redirect_to action: 'index'
-  # end
-
   def upload
     @upload_file = UploadFile.new( params.require(:upload_file).permit(:name, :file) )
     @upload_file.save
-    redirect_to action: "index"
+    redirect_to action: 'index'
   end
 
   def download
