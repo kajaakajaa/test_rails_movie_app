8d55a29f2ab4bd311596ad03d97e73b412905b56
kajaakajaa <rwqxh8703@yahoo.co.jp>
2020-02-29 22:52:47 +0900

    add_15mac_2-29

--- app/models/upload_file.rb
+++ app/models/upload_file.rb
@@ -1,5 +1,5 @@
 class UploadFile < ApplicationRecord
   mount_uploader :file, UploadFileUploader
-  validates :name, presence: true, length: { minimum: 15 }
-  validates :file, presence: true
+  validates :name, presence: true, length: { minimum: 15, message: "空欄では送信できません" }
+  validates :file, presence: { message: "空欄では送信できません" }
 end
